a124922a837b8e574bdc1eb4fc6c8c5ce412ad8e
Effy Zhang <effy@squareup.com>
2019-08-29 21:09:36 -0400

    add create

--- server.js
+++ server.js
@@ -2,20 +2,34 @@ const express = require('express');
 const path = require('path');
 const db = require('./db');
 const app = express();
- 
+
+const validator = (item, items) => {
+  console.log('hello!')
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'index.html' ));
 })
 
 app.get('/api/products', async (req, res, next) => {
     try{
-        res.send(await db('product.json', (item, items) => console.log('hello!')).findAll());
+        res.send(await db('product.json', validator).findAll());
     }
     catch(ex){
         next(ex);
-    } 
+    }
+  })
+
+  app.post('/api/products', async (req, res, next) => {
+    console.log(req)
+    try{
+        res.send(await db('product.json', validator).create(req.body));
+    }
+    catch(ex){
+        next(ex);
+    }
   })
 
 
- 
+
 app.listen(3000, () => console.log('listening on 3000'));
